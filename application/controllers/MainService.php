9bd098dbe5b465d23f8a7f699a5d6466546c7473
tantrieuf31 <tantrieuf31@73056f84-74e9-11de-ad01-0109e4891561>
2009-08-27 03:24:25 +0000

    commit

--- application/controllers/MainService.php
+++ application/controllers/MainService.php
@@ -5,85 +5,85 @@
  * @author Trieu Nguyen
  */
 class MainService extends Controller {
-	private $dev_mode = false;
-	function __construct() {
-		parent::Controller();
-		$this->load->helper('url');
-	}
+    private $dev_mode = false;
+    function __construct() {
+        parent::Controller();
+        $this->load->helper('url');
+    }
 
-	public function index() {
-		$this->load->view('test_ajax_service');
-	}
-	
-	public function testCrossSite($a,$callback) {
+    public function index() {
+        $this->load->view('test_ajax_service');
+    }
+
+    public function testCrossSite($a,$callback) {
         $answer = $a;
-        
-        if(is_string($answer)){        
-        	echo $callback."('".$answer."');";
+
+        if(is_string($answer)) {
+            echo $callback."('".$answer."');";
         }
-	    else if(is_object($answer)){        
-        	echo $callback."(".json_encode($answer).");";
+        else if(is_object($answer)) {
+                echo $callback."(".json_encode($answer).");";
+            }
+    }
+
+    public function handler() {
+        $service_name = $this->input->get_post("service_name");
+        $service_method = $this->input->get_post("service_method");
+        $params =  json_decode($this->input->get_post("service_method_params", TRUE), TRUE) ;
+        $service_id = $this->input->get_post("service_id");
+
+        if($this->dev_mode) {
+            echo($service_id);
+            foreach ($params   as $p) {
+                echo $p."<br>";
+            }
+        }
+        try {
+            require_once 'system/application/ajax_service/'.$service_name.".php";
+            $service = new ReflectionClass($service_name);
+            $method = $service->getMethod($service_method);
+
+            $method->invokeArgs($service,$params);
+        } catch (Exception $e) {
+            echo $e->getTraceAsString();
         }
     }
 
-	public function handler() {
-		$service_name = $this->input->get_post("service_name");
-		$service_method = $this->input->get_post("service_method");
-		$params =  json_decode($this->input->get_post("service_method_params", TRUE), TRUE) ;
-		$service_id = $this->input->get_post("service_id");
+    private function autoLoadModel() {
+        $classesDirectory = 'system/application/ajax_service/';
+        $classesExtension = '.php';
+        // require_once all classes in that directory
+        $d = dir($classesDirectory);
+        while (false !== ($entry = $d->read())) {
+        // Check extension
+            if (substr($entry, -(strlen($classesExtension))) == $classesExtension) {
+                require_once($classesDirectory.$entry);
+            //print_r($entry);
+            }
+        }
+        $d->close();
+    // print_r(get_declared_classes());;
+    }
 
-		if($this->dev_mode) {
-			echo($service_id);
-			foreach ($params   as $p) {
-				echo $p."<br>";
-			}
-		}
-		try {
-			require_once 'system/application/ajax_service/'.$service_name.".php";
-			$service = new ReflectionClass($service_name);
-			$method = $service->getMethod($service_method);
+    public static function  parseArrayToObject($array) {
+        $object = new stdClass();
+        if (is_array($array) && count($array) > 0) {
+            foreach ($array as $name=>$value) {
+                $name = strtolower(trim($name));
+                if (!empty($name)) {
+                    $object->$name = $value;
+                }
+            }
+        }
+        return $object;
+    }
 
-			$method->invokeArgs($service,$params);
-		} catch (Exception $e) {
-			echo $e->getTraceAsString();
-		}	
-	}
-
-	private function autoLoadModel() {
-		$classesDirectory = 'system/application/ajax_service/';
-		$classesExtension = '.php';
-		// require_once all classes in that directory
-		$d = dir($classesDirectory);
-		while (false !== ($entry = $d->read())) {
-			// Check extension
-			if (substr($entry, -(strlen($classesExtension))) == $classesExtension) {
-				require_once($classesDirectory.$entry);
-				//print_r($entry);
-			}
-		}
-		$d->close();
-		// print_r(get_declared_classes());;
-	}
-
-	public static function  parseArrayToObject($array) {
-		$object = new stdClass();
-		if (is_array($array) && count($array) > 0) {
-			foreach ($array as $name=>$value) {
-				$name = strtolower(trim($name));
-				if (!empty($name)) {
-					$object->$name = $value;
-				}
-			}
-		}
-		return $object;
-	}
-
-	public static function parseObjectToArray($object) {
-		$array = array();
-		if (is_object($object)) {
-			$array = get_object_vars($object);
-		}
-		return $array;
-	}
+    public static function parseObjectToArray($object) {
+        $array = array();
+        if (is_object($object)) {
+            $array = get_object_vars($object);
+        }
+        return $array;
+    }
 }
 ?>
