47e2abae839948587d6d76b565ae155ae92c3de8
tantrieuf31 <tantrieuf31@73056f84-74e9-11de-ad01-0109e4891561>
2009-09-17 06:37:42 +0000

    update ApplicationHook

---
+++ trunk/application/language/vietnamese/fields_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+//////////////////  Home page  ////////////////////
+$lang['home_page'] = "Trang chủ";
+$lang['job_seeker'] = "Người tìm việc";
+$lang['employer'] = "Việc tìm người";
+$lang['news_events'] = "Tin tức - Sự kiện";
+$lang['contact'] = "Liên hệ";
+
+?>
