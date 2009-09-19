a0068192122713668c9145cad52f75a79fb53f2c
tantrieuf31 <tantrieuf31@73056f84-74e9-11de-ad01-0109e4891561>
2009-09-19 04:17:58 +0000

    fix bugs when using PHP 5.3

--- trunk/application/config/database.php
+++ trunk/application/config/database.php
@@ -38,8 +38,8 @@ $active_group = "default";
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "trieu";
-$db['default']['password'] = "1234";
+$db['default']['username'] = "root";
+$db['default']['password'] = "";
 $db['default']['database'] = "job_management";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
